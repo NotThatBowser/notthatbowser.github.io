ce40f7ba7b0ed47c401acfdeefe72653962e49dd
= <=>
2022-04-23 12:57:35 +0100

    Add tags to header.

--- src/components/Profile/Header/Header.tsx
+++ src/components/Profile/Header/Header.tsx
@@ -1,7 +1,12 @@
+import Tags from "../../common/Tags";
+import { devStory, DevStory } from "../../../data/dev-story";
 import { ReactComponent as GitHubLogo } from "./github-brands.svg";
 import { ReactComponent as LinkedInLogo } from "./linkedin-brands.svg";
+import { useMemo } from "react";
 
 export default function Header() {
+  const uniqueTags = useMemo(() => getUniqueTags(devStory), []);
+
   return (
     <header className="text-center">
       <h1 className="mt-8 text-4xl font-inter">Max Bowser</h1>
@@ -13,6 +18,21 @@ export default function Header() {
           <LinkedInLogo title="LinkedIn" width={32} height={32} />
         </a>
       </div>
+      <Tags className="mt-4 px-4" tags={uniqueTags} />
     </header>
   );
+}
+
+function getUniqueTags(devStory: DevStory): string[] {
+  const experienceTags: string[] = devStory.experiences
+    .map((experience) => experience.tags)
+    .reduce((a, b) => a.concat(b));
+  const projectTags: string[] = devStory.projects
+    .map((project) => project.tags)
+    .reduce((a, b) => a.concat(b));
+  const allTags = experienceTags.concat(projectTags);
+  const uniqueTags = Array
+    .from(new Set(allTags))
+    .sort((a, b) => a.localeCompare(b));
+  return uniqueTags;
 }
