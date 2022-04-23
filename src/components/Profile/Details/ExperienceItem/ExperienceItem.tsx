194a4f8d646af18370ac0400ad108ac638abd573
= <=>
2022-04-23 13:07:40 +0100

    Update layout spacing.

--- src/components/Profile/Details/ExperienceItem/ExperienceItem.tsx
+++ src/components/Profile/Details/ExperienceItem/ExperienceItem.tsx
@@ -5,13 +5,13 @@ export default function ExperienceItem(experience: Experience) {
   const yearsString = `${experience.yearStart}-${experience.yearEnd ?? ""}`;
 
   return (
-    <div data-testid="experience-item" className="mb-2">
+    <div data-testid="experience-item" className="mb-4">
       <div>
         <div>
           <span className="font-inter">{experience.what}</span>
           <span className="text-xs"> ({yearsString})</span>
         </div>
-        <div className="text-xs text-slate-800">{experience.where}</div>
+        <div className="text-sm text-slate-700 mb-1">{experience.where}</div>
       </div>
       <Tags tags={experience.tags} />
     </div>
--- src/components/Profile/Details/ProjectItem/ProjectItem.tsx
+++ src/components/Profile/Details/ProjectItem/ProjectItem.tsx
@@ -3,9 +3,9 @@ import { Project } from "../../../../data/dev-story";
 
 export default function ProjectItem(project: Project) {
   return (
-    <div data-testid="project-item" className="mb-2">
+    <div data-testid="project-item" className="mb-4">
       <div>
-        <span className="font-inter">{project.what}</span>
+        <span className="font-inter text-md">{project.what}</span>
         {project.link && (
           <a href={project.link} title={project.link}>
             <sup>*</sup>
@@ -13,7 +13,7 @@ export default function ProjectItem(project: Project) {
         )}
         <span className="text-xs"> ({project.year})</span>
       </div>
-      <div className="text-sm text-slate-800">{project.about}</div>
+      <div className="text-sm text-slate-700 mb-1">{project.about}</div>
       <Tags tags={project.tags} />
     </div>
   );
